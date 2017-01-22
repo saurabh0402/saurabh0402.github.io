06fd7246bf4d7125407f0b3348f0ce30284760f5
saurabh0402 <saurabh.k0402@gmail.com>
2017-01-23 00:14:09 +0530

    new design for the website

---
+++ js/main.js
@@ -0,0 +1,42 @@
+(function(){
+
+  /* SKROLLR SETUP */
+
+  function adjust(){
+    var width = window.innerWidth,
+      height = window.innerHeight;
+
+    if(width >= 750){
+      var s = skrollr.init({
+        forceHeight: false
+      });
+    }
+
+    else {
+      var s = skrollr.init();
+      s.destroy();
+    }
+  }
+
+  var adjustObj = {
+    match: function(){
+      adjust();
+    },
+    unmatch: function(){
+      adjust();
+    }
+  };
+
+  /* THAT'S ALL THE SETUP I NEED FOR RESPONSIVE SKROLLR */
+
+  window.onload = function(){
+    setTimeout(function(){
+      document.getElementsByClassName("monitor")[0].className = "monitor";
+      document.getElementsByClassName("keyboard")[0].className = "keyboard";
+      document.getElementsByClassName("earphone")[0].className = "earphone";
+      document.getElementsByClassName("docs")[0].className = "docs";
+      enquire.register("screen and (min-width: 750px)", adjustObj, false);
+    }, 300);
+  }
+
+})();
