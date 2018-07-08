0d6bfa3647d95732cec7df4e0b3fe13dd72c0a4d
saurabh0402 <saurabh.k0402@gmail.com>
2018-07-08 22:18:31 +0530

    Eye blink

--- revamp/js/app.js
+++ revamp/js/app.js
@@ -1,4 +1,5 @@
 let blink = false;
+let hand = false;
 
 setTimeout(function(){
 	f();
@@ -6,20 +7,21 @@ setTimeout(function(){
 
 
 function f(){
-	let t = document.getElementById('rect1028');
-	let u = document.getElementById('rect1030');
+	let t = document.getElementById('rect937');
+	let u = document.getElementById('rect939');
+	
 	if(blink){
 		t.setAttribute('height', 8);
-		t.setAttribute('y', 42.756592);
+		t.setAttribute('y', 63.676292);
 		u.setAttribute('height', 8);
-		u.setAttribute('y', 42.756592);
+		u.setAttribute('y', 63.676292);
 		blink = false;
 		setTimeout(f, 2000);
 	} else {
 		t.setAttribute('height', 4);
-		t.setAttribute('y', 46.756592);
+		t.setAttribute('y', 67.676292);
 		u.setAttribute('height', 4);
-		u.setAttribute('y', 46.756592);
+		u.setAttribute('y', 67.676292);
 		blink = true;
 		setTimeout(f, 200);
 	}
