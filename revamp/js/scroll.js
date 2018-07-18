1cebf887f1c4e9134296106bbd74d196c89b987b
saurabh0402 <saurabh.k0402@gmail.com>
2018-07-18 14:14:30 +0530

    Fixed images on scroll

---
+++ revamp/js/scroll.js
@@ -0,0 +1,57 @@
+(function(){	
+	let conts = [];
+	let resizeId;
+
+	document.getElementsByClassName('container')[0].addEventListener('scroll', function(){
+		//console.log("In!");
+		//console.log(this.scrollTop);
+		[].forEach.call(document.getElementsByClassName('image-cont'), (e, i) => {
+			if(i == 0){
+				let t = conts[i];
+				if(this.scrollTop > 0){
+					if(this.scrollTop > 0 && this.scrollTop <= t.bottom - window.innerHeight){
+						e.style.top = this.scrollTop + "px";
+					}
+				}
+			} else {
+				let t = conts[i], u = conts[i-1];
+				if(this.scrollTop > u.bottom - window.innerHeight){
+					if(this.scrollTop > t.top && this.scrollTop <= t.bottom - window.innerHeight){
+						console.log("In!");
+						e.style.top = this.scrollTop + "px";
+					}
+				}	
+			}
+		});
+
+	});
+
+	function init(){
+		while(conts.length > 0){
+			conts.pop();
+		}
+
+		[].forEach.call(document.getElementsByClassName('image-cont'), function(e, i){
+			conts.push(document.getElementsByClassName('cont')[i].getBoundingClientRect());
+
+			if(i == 0){
+				e.style.top = "0px";
+			} else {
+				let t = document.getElementsByClassName('cont')[i-1].getBoundingClientRect();
+				e.style.top = t.top + t.height + "px";
+			}
+		});
+	}
+
+	function resize(){
+		let temp = document.getElementsByClassName('container')[0].scrollTop;
+		document.getElementsByClassName('container')[0].scrollTo(0,0);
+		init();
+	}
+
+	window.addEventListener('load', init);
+	window.addEventListener('resize', function(){
+		clearTimeout(resizeId);
+		resizeId = setTimeout(resize, 500);
+	});
+})();
