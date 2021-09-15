1a5f156f1947342ad5446db739dabc3a211e5af7
Saurabh Kumar <saurabh.k0402@gmail.com>
2021-09-15 20:19:39 +0530

    Update with latest data

--- js/append-skills.js
+++ js/append-skills.js
@@ -1,92 +1,112 @@
-(function(){
-	let skills = [{
-			title: 'Web Technologies',
-			list: [{
-				name: 'HTML',
-				rating: 70
-			}, {
-				name: 'CSS',
-				rating: 70
-			}, {
-				name: 'JavaScript',
-				rating: 85
-			}, {
-				name: 'Node.js',
-				rating: 75
-			}, {
-				name: 'React.js',
-				rating: 40
-			}]
-		}, {
-			title: 'Languages',
-			list: [{
-				name: 'C',
-				rating: 50
-			}, {
-				name: 'C++',
-				rating: 50
-			}, {
-				name: 'Python',
-				rating: 60
-			}]
-		}, {
-			title: 'Others',
-			list: [{
-				name: 'MongoDB',
-				rating: 50
-			}, {
-				name: 'SQL',
-				rating: 70
-			}, {
-				name: 'Git',
-				rating: 40
-			}, {
-				name: 'Inkscape',
-				rating: 60
-			}, {
-				name: 'Linux',
-				rating: 40
-			}]
-		}];
+(function () {
+  let skills = [
+    {
+      title: 'Web Technologies',
+      list: [
+        {
+          name: 'HTML',
+          rating: 70,
+        },
+        {
+          name: 'CSS',
+          rating: 70,
+        },
+        {
+          name: 'JavaScript',
+          rating: 75,
+        },
+        {
+          name: 'Node.js',
+          rating: 75,
+        },
+        {
+          name: 'React.js',
+          rating: 70,
+        },
+      ],
+    },
+    {
+      title: 'Languages',
+      list: [
+        {
+          name: 'JavaScript',
+          rating: 75,
+        },
+        {
+          name: 'Python',
+          rating: 60,
+        },
+      ],
+    },
+    {
+      title: 'Others',
+      list: [
+        {
+          name: 'SQL',
+          rating: 70,
+        },
+        {
+          name: 'Git',
+          rating: 40,
+        },
+        {
+          name: 'Docker',
+          rating: 50,
+        },
+        {
+          name: 'Kubernetes',
+          rating: 30,
+        },
+        {
+          name: 'Jest',
+          rating: 40,
+        },
+        {
+          name: 'Linux',
+          rating: 40,
+        },
+      ],
+    },
+  ];
 
-		let elems = skills.map(function(aSkillList){
-			let cont = document.createElement('div');
-			cont.className = "skills-sub";
-			let skillsTitle = document.createElement('div');
-			skillsTitle.className = "skills-title";
-			skillsTitle.appendChild(document.createTextNode(aSkillList.title));
-			cont.appendChild(skillsTitle);
+  let elems = skills.map(function (aSkillList) {
+    let cont = document.createElement('div');
+    cont.className = 'skills-sub';
+    let skillsTitle = document.createElement('div');
+    skillsTitle.className = 'skills-title';
+    skillsTitle.appendChild(document.createTextNode(aSkillList.title));
+    cont.appendChild(skillsTitle);
 
-			let skillsList = document.createElement('div');
-			skillsList.className = "skills-list";
+    let skillsList = document.createElement('div');
+    skillsList.className = 'skills-list';
 
-			aSkillList.list.forEach(function(aSkillObj){
-				let aSkill = document.createElement('div');
-				aSkill.className = "a-skill";
+    aSkillList.list.forEach(function (aSkillObj) {
+      let aSkill = document.createElement('div');
+      aSkill.className = 'a-skill';
 
-				let skillName = document.createElement('div');
-				skillName.className = "skill-name";
-				skillName.appendChild(document.createTextNode(aSkillObj.name));
+      let skillName = document.createElement('div');
+      skillName.className = 'skill-name';
+      skillName.appendChild(document.createTextNode(aSkillObj.name));
 
-				let skillRating = document.createElement('div');
-				skillRating.className = "skill-rating";
+      let skillRating = document.createElement('div');
+      skillRating.className = 'skill-rating';
 
-				let skillBoxOuter = document.createElement('div');
-				skillBoxOuter.className = "skill-box-outer";
+      let skillBoxOuter = document.createElement('div');
+      skillBoxOuter.className = 'skill-box-outer';
 
-				let skillBoxInner = document.createElement('div');
-				skillBoxInner.className = "skill-box-inner";
-				skillBoxInner.style.width = aSkillObj.rating + "%";
+      let skillBoxInner = document.createElement('div');
+      skillBoxInner.className = 'skill-box-inner';
+      skillBoxInner.style.width = aSkillObj.rating + '%';
 
-				skillBoxOuter.appendChild(skillBoxInner);
-				skillRating.appendChild(skillBoxOuter);
+      skillBoxOuter.appendChild(skillBoxInner);
+      skillRating.appendChild(skillBoxOuter);
 
-				aSkill.appendChild(skillName);
-				aSkill.appendChild(skillRating);
-				skillsList.appendChild(aSkill);
-			});
+      aSkill.appendChild(skillName);
+      aSkill.appendChild(skillRating);
+      skillsList.appendChild(aSkill);
+    });
 
-			cont.appendChild(skillsList);
-			document.getElementsByClassName('skills-text-content')[0].appendChild(cont);
-		});
+    cont.appendChild(skillsList);
+    document.getElementsByClassName('skills-text-content')[0].appendChild(cont);
+  });
 })();
